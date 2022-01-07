511d484b43edad6972b254bce237765ea08b90a7
Jeffrey Lent <jefflent01@gmail.com>
2022-01-07 10:50:27 -0500

    added mockmvc tests

--- src/main/java/org/emeritus/simpleservice/SimpleController.java
+++ src/main/java/org/emeritus/simpleservice/SimpleController.java
@@ -13,16 +13,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SimpleController {
 
-    HashMap<Integer, Book> books = new HashMap<>();
+    private SimpleService simpleService;
+
+    public SimpleController(SimpleService simpleService) {
+        this.simpleService = simpleService;
+    }
 
     @GetMapping(path = "/book/{id}")
     public ResponseEntity<Book> getBookById(@PathVariable("id") final String id) {
 
-        return new ResponseEntity<>(books.get(Integer.parseInt(id)), HttpStatus.OK);
+        return new ResponseEntity<>(simpleService.getBook(Integer.parseInt(id)), HttpStatus.OK);
     }
 
     @PostMapping(path = "/book")
     public ResponseEntity<Book> createBook(@RequestBody final Book book) {
-        return new ResponseEntity<>(books.put(book.getBookId(),book), HttpStatus.CREATED);
+        return new ResponseEntity<>(simpleService.addBook(book), HttpStatus.CREATED);
     }
 }
--- src/test/java/org/emeritus/simpleservice/SimpleControllerTests.java
+++ src/test/java/org/emeritus/simpleservice/SimpleControllerTests.java
@@ -1,11 +1,13 @@
 package org.emeritus.simpleservice;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
@@ -21,10 +23,14 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 @SpringBootTest
 public class SimpleControllerTests {
-    @Autowired
-    private WebApplicationContext context;
+    @Autowired private WebApplicationContext context;
+
+
+    @MockBean private SimpleService simpleServiceMock;
 
     protected MockMvc mvc;
 
@@ -36,7 +42,41 @@ public class SimpleControllerTests {
     }
     @Test
     void postEndPointTest() throws Exception {
-        //TODO: write MOCK MVC tests
+
+        Book book = new Book();
+        book.setAuthor("J.R.R. Tolkein");
+        book.setName("The Hobbit");
+        book.setBookId(123);
+
+        when(simpleServiceMock.addBook(any(Book.class))).thenReturn(book);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String json = objectMapper.writeValueAsString(book);
+
+        mvc.perform(post("/book")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json) 
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.bookId").value("123")) 
+                .andExpect(jsonPath("$.author").value("J.R.R. Tolkein"))
+                .andExpect(jsonPath("$.name").value("The Hobbit")); 
+
     }
 
+    @Test
+    void getEndPointTest() throws Exception {
+        Book book = new Book();
+        book.setAuthor("J.R.R. Tolkein");
+        book.setName("The Hobbit");
+        book.setBookId(123);
+
+        when(simpleServiceMock.getBook(123)).thenReturn(book);
+
+        mvc.perform(get("/book/123")
+            .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+    }
 }
